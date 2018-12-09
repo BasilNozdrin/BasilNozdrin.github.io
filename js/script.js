0468060c5c48c1c9279e4fc5e84e1eb2aced1455
BasilNozrin <bassvass@gmail.com>
2018-12-09 17:43:50 +0300

    real decomposition commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-//Borrowed function for "Set" type
 function union(setA, setB) {
     var _union = new Set(setA);
     for (var elem of setB) {
         _union.add(elem);
     }
     return _union;
-}
+};
 //My Own built-in Functions
+function cutter(i1, i2,word) {
+        let result = ""
+        if (i2 - i1 !== 1 ) {
+            result = word.slice(i1+1,i2);
+        };
+        return result;
+    };//returns substring from the word using indexes i1 and i2 without including them
 function isOpChar(a,b) {
     if (a.toLowerCase() === b.toLowerCase()){
         if (a.toLowerCase() == a) {
@@ -18,6 +24,18 @@ function isOpChar(a,b) {
         };
     } else {return false;};
 };//Check are the letters a and b opposite or not
+function opWord(word) {
+	let result = "";
+	for (let char_idx = word.length - 1; char_idx >= 0; char_idx--) {
+		let char = word.charAt(char_idx);
+		if (char === char.toUpperCase()) {
+			result += char.toLowerCase();
+		} else {
+			result += char.toUpperCase();
+		};
+	};
+	return result;
+};//Makes an opposite word from the word
 function red(word){
     let out = "";
     function innerRed(word,i){
@@ -39,18 +57,11 @@ function red(word){
     if (out !== ""){
         return out;
     } else {
-        return "1"
+        return ""
     };
 };//Applies reduction on the word
 function fialkDecompose(word){
     let result = new Set(null);
-    function cutter(i1, i2) {
-        let result = ""
-        if (i2 - i1 !== 1 ) {
-            result = word.slice(i1+1,i2);
-        };
-        return result;
-    };
     if (word.length < 4) {
         result.add("1");
     } else {
@@ -59,11 +70,11 @@ function fialkDecompose(word){
                 for (let a2_id = b1_id+1; a2_id <= word.length - 2; a2_id++) {
                     for (let b2_id = a2_id+1; b2_id <= word.length - 1; b2_id++) {
                         if (isOpChar(word[a1_id],word[a2_id]) && isOpChar(word[b1_id],word[b2_id])) {
-                            let w1 = cutter(-1,a1_id);
-                            let w2 = cutter(a1_id,b1_id);
-                            let w3 = cutter(b1_id,a2_id);
-                            let w4 = cutter(a2_id,b2_id);
-                            let w5 = cutter(b2_id,word.length);
+                            let w1 = cutter(-1,a1_id,word);
+                            let w2 = cutter(a1_id,b1_id,word);
+                            let w3 = cutter(b1_id,a2_id,word);
+                            let w4 = cutter(a2_id,b2_id,word);
+                            let w5 = cutter(b2_id,word.length,word);
                             result.add(red(w1+w4+w3+w2+w5));
                         };
                     };
@@ -87,26 +98,66 @@ function cl(word){
     };
     return i;
 };//Calculates commutator length of the word using two previous functions and calculating iterations
+function fialkDecomposition(word){
+    let result = new Set(null);
+    let separator = word.indexOf(":");
+    let tail = "";
+    let head = "";
+    if (separator === -1){
+        tail = "";
+        head = word;
+    } else if (separator = word.length){
+        tail = word.slice(0,separator);
+        head = "";
+    } else {
+        tail = word.slice(0,separator);
+        head = word.slice(separator+1,word.length);
+    };
+    if (head === "") {
+    result.add(tail);
+    } else {
+        for (let a1_id = 0; a1_id <= head.length - 4; a1_id++) {
+            for (let b1_id = a1_id+1; b1_id <= head.length - 3; b1_id++) {
+                for (let a2_id = b1_id+1; a2_id <= head.length - 2; a2_id++) {
+                    for (let b2_id = a2_id+1; b2_id <= head.length - 1; b2_id++) {
+                        if (isOpChar(head[a1_id],head[a2_id]) && isOpChar(head[b1_id],head[b2_id])) {
+                            let a = head[a1_id];
+                            let b = head[b1_id];
+                            let w1 = cutter(-1,a1_id,head);
+                            let w2 = cutter(a1_id,b1_id,head);
+                            let w3 = cutter(b1_id,a2_id,head);
+                            let w4 = cutter(a2_id,b2_id,head);
+                            let w5 = cutter(b2_id,head.length,head);
+                            result.add(tail+"["+red(w1+w4+w3+a+opWord(w1))+","+red(w1+w4+b+opWord(w2)+opWord(w3)+opWord(w4)+opWord(w1))+"]:"+red(w1+w4+w3+w2+w5));
+                        };
+                    };
+                };
+            };
+        };
+    };
+    return result;
+};// any word -> set of []:w1w4w3w2w5
+function massFialkDecomposition(set){
+    let result = new Set(null);
+    set.forEach(word => result = union(result,(fialkDecomposition(word))));
+    return result;
+};
+function cDecomposition(word){
+    flag = true
+    let set = fialkDecomposition(word);
+    result = "-1";
+    while (result === "-1") {
+        set = massFialkDecomposition(set);
+        function f(word){
+            if (word.indexOf(":") === (word.length - 1)) {
+                result = word.slice(0,-1);
+            };
+        };
+        set.forEach(word => f(word));
+    return result;
+    };
+};
 //My Own in-html-used Functions
-function opWordFunction() {
-	let opWordIn = document.getElementById("opWordInput_id");
-	let opWordOut = document.getElementById("opWordOutput_id");
-	let text = opWordIn.value;
-	let new_text = "";
-	for (let char_idx = text.length - 1; char_idx >= 0; char_idx--) {
-		let char = text.charAt(char_idx);
-		if (char.toUpperCase() === char.toLowerCase()) {
-			new_text = "Error";
-			break;
-		};
-		if (char === char.toUpperCase()) {
-			new_text += char.toLowerCase();
-		} else {
-			new_text += char.toUpperCase();
-		};
-	};
-	opWordOut.innerHTML = new_text;
-};//Makes an opposite word from the word
 function wordCheckFunction() {
     let wordCheckIn = document.getElementById("wordCheckInput_id");
     let wordCheckOut = document.getElementById("wordCheckOutput_id");
@@ -155,15 +206,7 @@ function fialkDecomposeFunction() {
     let fialkDecomposeFunctionIn = document.getElementById("fialkDecomposeFunctionIn_id");
     let fialkDecomposeFunctionOut = document.getElementById("fialkDecomposeFunctionOut_id");
     let in_value = fialkDecomposeFunctionIn.value;
-    fialkDecomposeFunctionOut.innerHTML = "wait...";
     let out_value = "";
-    function cutter(i1, i2) {
-        let result = ""
-        if (i2 - i1 != 1 ) {
-            result = in_value.slice(i1+1,i2);
-        };
-        return result;
-    };
     if (in_value.length < 4) {
         out_value = "bad word";
     };
@@ -172,13 +215,13 @@ function fialkDecomposeFunction() {
             for (let a2_id = b1_id+1; a2_id <= in_value.length - 2; a2_id++) {
                 for (let b2_id = a2_id+1; b2_id <= in_value.length - 1; b2_id++) {
                     if (isOpChar(in_value[a1_id],in_value[a2_id]) && isOpChar(in_value[b1_id],in_value[b2_id])) {
-                        let w1 = cutter(-1,a1_id);
-                        let w2 = cutter(a1_id,b1_id);
-                        let w3 = cutter(b1_id,a2_id);
-                        let w4 = cutter(a2_id,b2_id);
-                        let w5 = cutter(b2_id,in_value.length);
+                        let w1 = cutter(-1,a1_id,in_value);
+                        let w2 = cutter(a1_id,b1_id,in_value);
+                        let w3 = cutter(b1_id,a2_id,in_value);
+                        let w4 = cutter(a2_id,b2_id,in_value);
+                        let w5 = cutter(b2_id,in_value.length,in_value);
                         let newbie = w1+"("+in_value[a1_id]+")"+w2+"("+in_value[b1_id]+")"+w3+"("+in_value[a2_id]+")"+w4+"("+ in_value[b2_id]+")"+w5;
-                        out_value = out_value + newbie + "&#8195; ----> &#8195;" + red(w1+w4+w3+w2+w5) + "<br>";
+                        out_value = out_value + newbie + "&#8194;&#8658;&#8194;" + red(w1+w4+w3+w2+w5) + "<br>";
                     };
                 };
             };
@@ -193,43 +236,10 @@ function clFunction(){
     let out_value = new String(cl(in_value));
     clFunctionOut.innerHTML = "cl(" + in_value + ")=" + out_value;
 };//Calculates commutator length of the word
-//Borrowed meme functions
-function not_mine(){
-    let word1 = document.querySelector("#word1");
-    let word2 = document.querySelector("#word2");
-    let result = document.querySelector("#result");
-    function invert_word(word) {
-        let new_word = "";
-        for (let char_idx = word.length - 1; char_idx >= 0; char_idx--) {
-            let char = word.charAt(char_idx);
-            if (char === char.toUpperCase()) {
-                new_word += char.toLowerCase();
-            } else {
-                new_word += char.toUpperCase();
-            };
-        };
-        return new_word;
-    };
-    function do_ur_work() {
-        let first = invert_word(word1.value);
-        let second = word2.value;
-        let shorter_len = 0;
-        if (first.length < second.length) {
-            shorter_len = first.length;
-        } else {
-            shorter_len = second.length;
-        };
-        while (true) {
-            if (first[0] === second[0]) {
-                first = first.slice(1);
-                second = second.slice(1);
-            } else {
-            break;
-            };
-        };
-        result.innerHTML = invert_word(first) + second;
-    };
-    word1.addEventListener('input', do_ur_work);
-    word2.addEventListener('input', do_ur_work);
-    };
-//not_mine();
+function fialkDecompositionFunction(){
+    let fialkDecompositionFunctionIn = document.getElementById("fialkDecompositionIn_id");
+    let fialkDecompositionFunctionOut = document.getElementById("fialkDecompositionOut_id");
+    let in_value = new String(fialkDecompositionFunctionIn.value);
+    let out_value = new String(fialkDecomposition(in_value));
+    fialkDecompositionFunctionOut.innerHTML = in_value + "&#8194;&#8658;&#8194;" + out_value;
+};//
